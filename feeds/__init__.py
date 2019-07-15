d64cac186699faa2c158783d4fcc048b168e30df
trevor grayson <trevor@retentionscience.com>
2019-07-14 17:45:11 -0700

    merge

--- feeds/__init__.py
+++ feeds/__init__.py
@@ -1,7 +1,11 @@
+import os
 import feedparser
 from yaml import load, Loader
+import ssl
+if hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-FEED_CONFIG = "/Users/tgrayson/.feeds/feeds.yml"
+FEED_CONFIG = "/".join((os.environ.get("HOME", '.'), ".feeds/feeds.yml"))
 
 
 def all_feeds():
@@ -17,8 +21,27 @@ def get_feed():
     for feed in all_feeds():
         NewsFeed = feedparser.parse(feed)
 
-        for entry in NewsFeed.entries:
-            print entry.title + " :: " + entry.published  
-            print "\t" + entry.link
-            # print "\t" + entry.summary
+        returns = {}
 
+        for entry in NewsFeed.entries:
+            project, status = entry.title.split('#')
+            returns[project] = returns.get(project, [])   
+            returns[project].append((entry.published, status, entry.link))
+
+        # need a preview
+        # need a method, returns tuple or string with params to print
+
+        for ret, items in returns.items():
+            row = max(items)
+
+            output = (
+                ret,
+                '\t\t\t',
+                row[2]
+            )
+
+            if ('success' not in row[1]):
+                output = ('[x]',) + output
+
+                print(''.join(output))
+            
