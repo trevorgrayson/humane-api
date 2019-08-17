d3c3bc99cb872136af21e0640ac6bd4d9c4fd32a
trevor grayson <trevor@trevorgrayson.com>
2019-08-17 07:49:57 -0700

    feeds/models.py

--- feeds/__init__.py
+++ feeds/__init__.py
@@ -1,64 +1,15 @@
 import os
 import feedparser
 import os
-import re
 from yaml import load, Loader
 import ssl
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
+from .models import Feed
 
 FEED_CONFIG = "/".join((os.environ.get("HOME", '.'), ".feeds/feeds.yml"))
 
 # hash in dir to see if read.
-
-class Feed:
-    def __init__(self, **kwargs):
-        self.url = kwargs.get('url')
-        self.status = kwargs.get('status')
-
-
-    def fetch(self):
-        self.entries = []
-
-        self.NewsFeed = feedparser.parse(self.url)
-        for entry in self.NewsFeed.entries:
-            status_regex = re.compile(self.status, re.MULTILINE)
-            status_match = re.search(status_regex, entry.summary)
-
-            status = status_match[0] if status_match else 'unkwn'
-
-            self.entries.append(Entry(
-              title=entry.title,
-              status=status,
-              link=entry.link,
-              timestamp=entry.updated
-            ))
-
-            if self.latest_only:
-                break
-
-
-    @property
-    def latest_only(self):
-        return self.status is not None
-
-
-class Entry:
-    def __init__(self, **kwargs):
-        self.title=kwargs.get('title')
-        self.status=kwargs.get('status')
-        self.link=kwargs.get('link')
-        self.timestamp=kwargs.get('timestamp')
-
-
-    def __str__(self):
-        build_success = re.compile("State: passed", re.MULTILINE)
-
-        return entry.title + "\n" +\
-            ("PASS" if re.search(build_success, entry.summary) else "FAIL") +\
-            "\t" + entry.link
-            # print "\t" + entry.summary
-
 
 
 
@@ -70,45 +21,23 @@ def all_feeds():
             yield url
 
 
+def print_line(entry):
+    headline = [entry.title]
+
+    if entry.status:
+        headline.append(entry.status)
+
+    headline.append(entry.timestamp)
+    headline.append(entry.link)
+
+    print(" :: ".join(headline))
+
+
 def get_feed():
 
     for feed in all_feeds():
         feed = Feed(**feed)
         feed.fetch()
 
-<<<<<<< HEAD
-        returns = {}
-
-        for entry in NewsFeed.entries:
-<<<<<<< HEAD
-            project, status = entry.title.split('#')
-            returns[project] = returns.get(project, [])   
-            returns[project].append((entry.published, status, entry.link))
-
-        # need a preview
-        # need a method, returns tuple or string with params to print
-
-        for ret, items in returns.items():
-            row = max(items)
-
-            output = (
-                ret,
-                '\t\t\t',
-                row[2]
-            )
-
-            if ('success' not in row[1]):
-                output = ('[x]',) + output
-=======
-            print(entry.title) # + " :: " + entry.published)
-=======
         for entry in feed.entries:
-            print(f"{entry.title} :: {entry.status} :: {entry.timestamp}")
-            # print(entry.summary)
->>>>>>> feeds modeling
-            print("\t" + entry.link)
-            # print "\t" + entry.summary
->>>>>>> moving towards python3
-
-                print(''.join(output))
-            
+            print_line(entry)
--- feeds/models.py
+++ feeds/models.py
@@ -1,6 +1,56 @@
-class News:
-    def __init__(self, **kwargs):
-        self.title = kwargs.get('title')
-        self.link = kwargs.get('link')
+import re
+import feedparser
 
-        self.source = None
+
+# source
+class Feed:
+    def __init__(self, **kwargs):
+        self.url = kwargs.get('url')
+        self.status = kwargs.get('status')
+
+
+    def fetch(self):
+        self.entries = []
+
+        self.NewsFeed = feedparser.parse(self.url)
+        for entry in self.NewsFeed.entries:
+            self.entries.append(Entry(
+              title=entry.title,
+              summary=entry.summary,
+              status=self.get_status(entry),
+              link=entry.link,
+              timestamp=entry.updated
+            ))
+
+            if self.latest_only:
+                break
+
+    def get_status(self, entry):
+        if self.status:
+            status_regex = re.compile(self.status, re.MULTILINE)
+            status_match = re.search(status_regex, entry.summary)
+
+            return status_match[0] if status_match else 'unkwn'
+
+
+    @property
+    def latest_only(self):
+        return self.status is not None
+
+
+class Entry:
+    def __init__(self, **kwargs):
+        self.title=kwargs.get('title')
+        self.summary=kwargs.get('summary')
+        self.status=kwargs.get('status')
+        self.link=kwargs.get('link')
+        self.timestamp=kwargs.get('timestamp')
+
+
+    def __str__(self):
+        build_success = re.compile("State: passed", re.MULTILINE)
+
+        return entry.title + "\n" +\
+            ("PASS" if re.search(build_success, entry.summary) else "FAIL") +\
+            "\t" + entry.link
+            # print "\t" + entry.summary
